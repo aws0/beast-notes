9654d3cfffd976fecd3d7907b10917210adc6d4e
aws0 <aalsamarrie@gmail.com>
2023-11-07 11:17:20 -0500

    Update audio-sink-cemu.sh
    cleanup comments

--- steamdeck-cemu-audio/audio-sink-cemu.sh
+++ steamdeck-cemu-audio/audio-sink-cemu.sh
@@ -3,7 +3,9 @@
 ## script that you can use before launching cemu (flatpak version) in steam deck to configure it to pickup the active audio channel (you can add it to the launch options)
 ## I use it to fix an annoying issue with ceum (flatpak version) where it doesnt use the current active audio SINK; so when you connect to TV via HDMI, audio continues to come through the steamdeck and not through TV via HDMI ! then you need to go to desktop mode and set the audio settings manaully to use HDMI
 ## this script automates this manual step
-## steam launch option example: /bin/bash -c '/home/deck/audio-sink-cemu.sh && /usr/bin/flatpak "run" "--branch=stable" "--arch=x86_64" "--command=/app/bin/Cemu-wrapper" "info.cemu.Cemu" -f -g "/path/to/game/file"'
+## TARGET: /usr/bin/bash
+## START IN:  /usr/bin
+## steam launch option example: -c '/home/deck/audio-sink-cemu.sh && /usr/bin/flatpak "run" "--branch=stable" "--arch=x86_64" "--command=/app/bin/Cemu-wrapper" "info.cemu.Cemu" -f -g "/path/to/game/file"'
 
 ## switch to the current active PA audio SINK
 export audioSinkACP=$(paplay /usr/share/sounds/Oxygen-Window-Maximize.ogg && pactl list short sinks | grep -e 'RUNNING' -e 'IDLE' | awk '{print $2}')
